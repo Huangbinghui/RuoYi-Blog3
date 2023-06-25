8edb75981f2741714831ab99196476fcc1258dec
huangbinghui <herberth.huang@icloud.com>
2023-06-25 23:34:11 +0800

    init

---
+++ src/views/blog/Blog.vue
@@ -0,0 +1,224 @@
+<script setup>
+
+import {getCurrentInstance, reactive, ref, toRefs} from "vue";
+import {listBlog} from "@/api/blog";
+
+const {proxy} = getCurrentInstance();
+const {blog_type, yon} = proxy.useDict("blog_type", "yon");
+
+const dateRange = ref([]);
+const showSearch = ref(true);
+const blogList = ref([]);
+const total = ref(0);
+const loading = ref(false);
+
+const data = reactive({
+  form: {},
+  queryParams: {
+    pageNum: 1,
+    pageSize: 10,
+    title: undefined,
+    blogType: undefined,
+    author: undefined,
+    isPrivate: undefined,
+    isOriginal: undefined,
+    isTop: undefined,
+    isDeleted: undefined
+  },
+  rules: {
+    roleName: [{required: true, message: "角色名称不能为空", trigger: "blur"}],
+    roleKey: [{required: true, message: "权限字符不能为空", trigger: "blur"}],
+    roleSort: [{required: true, message: "角色顺序不能为空", trigger: "blur"}]
+  },
+})
+
+const {form, queryParams, rules} = toRefs(data);
+
+function handleQuery() {
+  queryParams.value.pageNum = 1;
+  getList();
+  console.log(blogList.value);
+}
+
+function getList() {
+  loading.value = true;
+  listBlog(proxy.addDateRange(queryParams.value, dateRange.value)).then(response => {
+    blogList.value = response.rows;
+    total.value = response.total;
+    loading.value = false;
+  }).catch(error => {
+    proxy.$modal.msgError("查询失败！");
+    console.log(error);
+  })
+}
+
+function resetQuery(){
+  
+}
+
+function handleAdd(){
+  
+}
+
+function handleUpdate(){
+  
+}
+
+function handleDelete(){
+  
+}
+
+function handleExport(){
+  
+}
+
+function handleSelectionChange(){
+
+}
+</script>
+
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryRef" v-show="showSearch" :inline="true" label-width="68px">
+      <el-form-item label="标题" prop="title">
+        <el-input
+            v-model="queryParams.title"
+            placeholder="请输入标题！"
+            clearable
+            style="width: 240px"
+            @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="作者" prop="author">
+        <el-input
+            v-model="queryParams.author"
+            placeholder="请输入作者名称！"
+            clearable
+            style="width: 240px"
+            @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="类型" prop="blogType">
+        <el-select
+            v-model="queryParams.blogType"
+            placeholder="请选择博客类型！"
+            clearable
+            style="width: 240px">
+          <el-option
+              v-for="dict in blog_type"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"/>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="创建时间" style="width: 308px">
+        <el-date-picker
+            v-model="dateRange"
+            value-format="YYYY-MM-DD"
+            type="daterange"
+            range-separator="-"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-form-item>
+      <el-form-item label="是否原创" style="width: 240px">
+        <el-radio-group v-model="queryParams.isOriginal">
+          <el-radio
+              v-for="dict in yon"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value">
+            {{ dict.label }}
+          </el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="是否私密" style="width: 240px">
+        <el-radio-group v-model="queryParams.isPrivate">
+          <el-radio
+              v-for="dict in yon"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value">
+            {{ dict.label }}
+          </el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="是否置顶" style="width: 240px">
+        <el-radio-group v-model="queryParams.isTop">
+          <el-radio
+              v-for="dict in yon"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value">
+            {{ dict.label }}
+          </el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="是否删除" style="width: 240px">
+        <el-radio-group v-model="queryParams.isDeleted">
+          <el-radio
+              v-for="dict in yon"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value">
+            {{ dict.label }}
+          </el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+        <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+            type="primary"
+            plain
+            icon="Plus"
+            @click="handleAdd"
+            v-hasPermi="['blog:blog:add']"
+        >新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+            type="success"
+            plain
+            icon="Edit"
+            :disabled="single"
+            @click="handleUpdate"
+            v-hasPermi="['blog:blog:edit']"
+        >修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+            type="danger"
+            plain
+            icon="Delete"
+            :disabled="multiple"
+            @click="handleDelete"
+            v-hasPermi="['blog:blog:remove']"
+        >删除</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+            type="warning"
+            plain
+            icon="Download"
+            @click="handleExport"
+            v-hasPermi="['blog:blog:export']"
+        >导出</el-button>
+      </el-col>
+      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="blogList" @selection-change="handleSelectionChange">
+
+    </el-table>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -34,16 +34,16 @@
                />
             </el-select>
          </el-form-item>
-         <el-form-item label="创建时间" style="width: 308px">
-            <el-date-picker
-               v-model="dateRange"
-               value-format="YYYY-MM-DD"
-               type="daterange"
-               range-separator="-"
-               start-placeholder="开始日期"
-               end-placeholder="结束日期"
-            ></el-date-picker>
-         </el-form-item>
+        <el-form-item label="创建时间" style="width: 308px">
+          <el-date-picker
+              v-model="dateRange"
+              value-format="YYYY-MM-DD"
+              type="daterange"
+              range-separator="-"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+          ></el-date-picker>
+        </el-form-item>
          <el-form-item>
             <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
             <el-button icon="Refresh" @click="resetQuery">重置</el-button>
