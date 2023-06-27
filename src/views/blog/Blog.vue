699181945bbe7b428715806fef9d48b3d6c34ff0
huangbinghui <herberth.huang@icloud.com>
2023-06-27 13:48:11 +0800

    query

--- src/views/blog/Blog.vue
+++ src/views/blog/Blog.vue
@@ -7,10 +7,13 @@ const {proxy} = getCurrentInstance();
 const {blog_type, yon} = proxy.useDict("blog_type", "yon");
 
 const dateRange = ref([]);
-const showSearch = ref(true);
 const blogList = ref([]);
 const total = ref(0);
+
+const showSearch = ref(true);
 const loading = ref(false);
+const open = ref(false);
+const title = ref("");
 
 const data = reactive({
   form: {},
@@ -53,11 +56,47 @@ function getList() {
 }
 
 function resetQuery(){
-  
+  dateRange.value = [];
+  proxy.resetForm("queryRef");
+  handleQuery();
 }
 
 function handleAdd(){
-  
+  reset();
+  open.value = true;
+  title.value = "添加博客"
+}
+
+function reset(){
+  form.value = {
+    title: undefined,
+    blogType: undefined,
+    isPrivate: undefined,
+    isOriginal: undefined,
+    originalLink:undefined,
+    isTop: undefined,
+    isDeleted: undefined,
+    blog:{
+      content:undefined,
+    },
+  }
+  proxy.resetForm("blogRef");
+}
+
+function submitForm(){
+  proxy.$refs["blogRef"].validate(valid => {
+    if (valid){
+      if (form.value.id != undefined){
+
+      } else {
+
+      }
+    }
+  })
+}
+
+function cancel(){
+
 }
 
 function handleUpdate(){
@@ -214,8 +253,135 @@ function handleSelectionChange(){
     </el-row>
 
     <el-table v-loading="loading" :data="blogList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="标题" prop="title" :show-overflow-tooltip="true" width="120" />
+      <el-table-column label="博客类型" prop="blogType" width="150" />
+      <el-table-column label="作者" prop="author" width="150" />
+      <el-table-column label="原始链接" prop="originalLink" width="150" />
+      <el-table-column label="是否置顶" prop="isTop" width="150" />
+      <el-table-column label="是否原创" prop="isOriginal" width="150" />
+      <el-table-column label="是否私密" prop="isPrivate" width="150" />
+      <el-table-column label="创建时间" align="center" prop="createTime">
+        <template #default="scope">
+          <span>{{ parseTime(scope.row.createTime) }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template #default="scope">
+          <el-tooltip content="修改" placement="top" v-if="scope.row.roleId !== 1">
+            <el-button
+                type="text"
+                icon="Edit"
+                @click="handleUpdate(scope.row)"
+                v-hasPermi="['system:role:edit']"
+            ></el-button>
+          </el-tooltip>
+          <el-tooltip content="删除" placement="top" v-if="scope.row.roleId !== 1">
+            <el-button
+                type="text"
+                icon="Delete"
+                @click="handleDelete(scope.row)"
+                v-hasPermi="['system:role:remove']"
+            ></el-button>
+          </el-tooltip>
+          <el-tooltip content="置顶" placement="top" v-if="scope.row.roleId !== 1">
+            <el-button
+                type="text"
+                icon="CircleCheck"
+                @click="handleTop(scope.row)"
+                v-hasPermi="['system:role:edit']"
+            ></el-button>
+          </el-tooltip>
+          <el-tooltip content="查看详情" placement="top" v-if="scope.row.roleId !== 1">
+            <el-button
+                type="text"
+                icon="User"
+                @click="handleBlogInfo(scope.row)"
+                v-hasPermi="['system:role:edit']"
+            ></el-button>
+          </el-tooltip>
+        </template>
+      </el-table-column>
 
     </el-table>
+
+    <pagination
+        v-show="total > 0"
+        :total="total"
+        v-model:page="queryParams.pageNum"
+        v-model:limit="queryParams.pageSize"
+        @pagination="getList"
+    />
+
+    <el-dialog :title="title" v-model="open" width="500px" append-to-body>
+      <el-form ref="blogRef" :model="form" :rules="rules" label-width="100px" >
+        <el-form-item label="标题" prop="title" >
+          <el-input v-model="form.title" placeholder="请输入标题！"/>
+        </el-form-item>
+        <el-form-item label="类型" prop="blogType">
+          <el-select
+              v-model="form.blogType"
+              placeholder="请选择博客类型！"
+              clearable
+              style="width: 240px">
+            <el-option
+                v-for="dict in blog_type"
+                :key="dict.value"
+                :label="dict.label"
+                :value="dict.value"/>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="是否原创" style="width: 240px">
+          <el-radio-group v-model="form.isOriginal">
+            <el-radio
+                v-for="dict in yon"
+                :key="dict.value"
+                :label="dict.label"
+                :value="dict.value">
+              {{ dict.label }}
+            </el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="是否私密" style="width: 240px">
+          <el-radio-group v-model="form.isPrivate">
+            <el-radio
+                v-for="dict in yon"
+                :key="dict.value"
+                :label="dict.label"
+                :value="dict.value">
+              {{ dict.label }}
+            </el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="是否置顶" style="width: 240px">
+          <el-radio-group v-model="form.isTop">
+            <el-radio
+                v-for="dict in yon"
+                :key="dict.value"
+                :label="dict.label"
+                :value="dict.value">
+              {{ dict.label }}
+            </el-radio>
+          </el-radio-group>
+        </el-form-item>
+      </el-form>
+      <el-form-item label="原始链接" prop="originalLink"  v-if="form.isOriginal == false">
+        <el-input v-model="form.originalLink" placeholder="请输入原始链接！"/>
+      </el-form-item>
+      <el-form-item label="内容" prop="blog.content">
+        <el-input v-model="form.blog.content" placeholder="请输入博客内容！" />
+      </el-form-item>
+
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="submitForm">确 定</el-button>
+          <el-button @click="cancel">取 消</el-button>
+        </div>
+      </template>
+
+    </el-dialog>
+
   </div>
 </template>
 
